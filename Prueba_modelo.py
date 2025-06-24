4fe25870a005b52d4d63416031a4d088a6153489
Juan M <juangrigolatto1603@hotmail.com>
2025-06-23 22:40:55 -0300

    Ignorar archivo pesado definitivamente

--- Prueba_modelo.py
+++ Prueba_modelo.py
@@ -1,11 +1,31 @@
 import torch
 from Modelos.Modelo_conv import Modelo_Convolucional
+from Clase_UCIDataset import UCIDataset
+import os
 
+def main():
 
-path='best_model.pt'
+    parameters = {
+            'batch_size': 20,
+            'shuffle': True,
+            'num_workers': 2,
+            'pin_memory': True
+        }
 
-model=Modelo_Convolucional(in_channels=2,out_channels=2, long_signal=250)
+    print(os.path.exists('data_UCI/dataset_completo_prueba.pt'))
+    dataset = UCIDataset('data_UCI/dataset_completo_prueba.pt')
+    print(len(dataset))
+    dataset=dataset[:(20)]
+    print(len(dataset[1]))
+    dataloader = torch.utils.data.DataLoader(dataset)
+    
+    path_model='best_model.pt'
 
-checkpoint = torch.load(path)
-model.load_state_dict(checkpoint['model_state_dict'])
-model.eval()
+    model=Modelo_Convolucional(in_channels=2,out_channels=2, long_signal=250)
+    
+    checkpoint = torch.load(path_model)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.eval()
+
+if __name__ == '__main__':
+    main()

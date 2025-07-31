f97141c991d3f2004e3e998977ccee6c39b5d96e
Juan M <juangrigolatto1603@hotmail.com>
2025-07-31 01:50:22 -0300

    Se actualiza a entrenamiento de modelos sin data augmentation

--- Entrenamiento.py
+++ Entrenamiento.py
@@ -1,200 +1,227 @@
-#%%
 import torch
 from Clase_UCIDataset import UCIDataset
 import os
 from torch.utils import data
+from torch.utils.data import TensorDataset, random_split
 import numpy as np
 #from Modelos.InceptionTime import InceptionTime
-from Modelos.Modelo_conv import Modelo_Convolucional
+#from Modelos.Modelo_conv import Modelo_Convolucional
+from Modelos.ConvolucionalV1 import Modelo_ConvolucionalV1
+#from Modelos.ConvolucionalV2 import Modelo_ConvolucionalV2
 from tqdm.auto import tqdm 
 import matplotlib.pyplot as plt
-#%%
-use_cuda = torch.cuda.is_available()
-device = torch.device("cuda:0" if use_cuda else "cpu")
-print(device)
-#%%
-"""
-# Versión optimizada que precarga en RAM
-class RAMDataset(data.Dataset):
-    def __init__(self, uci_dataset):
-        #Precarga en RAM los datos de un UCIDataset
-        self.data = []
-        self.labels = []
-        
-        print("Precargando datos en RAM...")
-        for i in tqdm(range(len(uci_dataset)), desc="Cargando datos"):
-            x, y = uci_dataset[i]
-            self.data.append(x)
-            self.labels.append(y)
-        
-        # Convertimos a tensores
-        self.data = torch.stack(self.data)
-        self.labels = torch.stack(self.labels)
-    
-    def __len__(self):
-        return len(self.data)
-    
-    def __getitem__(self, index):
-        return self.data[index], self.labels[index]
-"""    
-parameters = {
-    'batch_size': 64,
-    'shuffle': True,
-    'num_workers': 2,
-    'pin_memory': True
-}    
-if __name__ == '__main__':
-    # Obtener los IDs de los archivos
-    data_dir = 'datos_UCI'
-    all_IDs = [f[:-3] for f in os.listdir(data_dir) if f.endswith('.pt')]
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import random
 
-    #  Aleatorizar los IDs
-    np.random.shuffle(all_IDs)
+def set_seed(seed=24):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # para multi-GPU
 
-    # Crear la partición (80% train, 20% validation)
-    train_size = int(0.8 * len(all_IDs))
-    partition = {
-        'train': all_IDs[:train_size],
-        'validation': all_IDs[train_size:]
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def main():
+    # Configuración del dispositivo
+    parameters = {
+        'batch_size': 256,
+        'shuffle': True,
+        'num_workers': 0,
+        'pin_memory': False
     }
+    set_seed(42)
     """
-        # Crear datasets y precargar en RAM
-    print("\nPrecargando conjunto de entrenamiento...")
-    train_dataset = RAMDataset(UCIDataset(partition['train'], data_dir))
+    print(os.path.exists('data_UCI/dataset_completo.pt'))
+    dataset = UCIDataset('data_UCI/dataset_completo.pt')
     
-    print("\nPrecargando conjunto de validación...")
-    val_dataset = RAMDataset(UCIDataset(partition['validation'], data_dir))
-
-    # Crear DataLoaders
-    train_loader = torch.utils.data.DataLoader(train_dataset, **parameters)
-    val_loader = torch.utils.data.DataLoader(val_dataset, **parameters)
-
-    # Verificación de uso de memoria
-    print(f"\nMemoria usada por datos de entrenamiento: {train_dataset.data.element_size() * train_dataset.data.nelement() / (1024**2):.2f} MB")
-    print(f"Memoria usada por datos de validación: {val_dataset.data.element_size() * val_dataset.data.nelement() / (1024**2):.2f} MB")
-    """
-
-    training_set = UCIDataset(partition['train'], data_dir=data_dir)	
+    
+    train_size = int(0.8 * len(dataset))
+    val_size = len(dataset) - train_size
+    
+    training_set, validation_set = random_split(dataset, [train_size, val_size])
+    
     training_generator = torch.utils.data.DataLoader(training_set, **parameters)
-    print(f"Conjunto de entrenamiento: {len(training_set)} muestras")   
-    print(training_set[0][0])
-    validation_set = UCIDataset(partition['validation'], data_dir=data_dir)
     validation_generator = torch.utils.data.DataLoader(validation_set, **parameters)
-    print(f"Conjunto de validacion: {len(validation_set)} muestras")  
-    print(validation_set[0][0])
+    """
+    archivos = [
+    'data_UCI/dataset_parte_1.pt',
+    'data_UCI/dataset_parte_2.pt',
+    'data_UCI/dataset_parte_3.pt',
+    'data_UCI/dataset_parte_4.pt',
+    ]
 
-    
+    dataset_completo = UCIDataset(archivos)
+
+    total = len(dataset_completo)
+    train_size = int(0.7 * total)
+    val_size = int(0.2 * total)
+    test_size = total - train_size - val_size
+
+    # Dividir aleatoriamente el dataset
+    train_set, val_set, test_set = random_split(dataset_completo, [train_size, val_size, test_size])
 
 
-    # Obtener un solo batch del DataLoader
-    for batch in training_generator:
-        inputs, labels = batch
-    
-        # Mostrar tamaño del batch
-        print("Tamaño de inputs:", inputs.shape)
-        print("Tamaño de labels:", labels.shape)
-    
-        # Visualizar las primeras señales del batch (ejemplo para señales de 2 canales y 250 muestras)
-        for i in range(3):  # Mostrar 3 ejemplos
-            plt.figure(figsize=(10, 3))
-            plt.title(f"Ejemplo {i+1} - Señales PPG y ECG")
-            plt.plot(inputs[i][0].cpu().numpy(), label='ppg')
-            plt.plot(inputs[i][1].cpu().numpy(), label='ecg')
-            plt.legend()
-            plt.show()
-    
-        break  # Solo queremos un batch para revisar
+    print(f"Train: {len(train_set)}, Val: {len(val_set)}, Test: {len(test_set)}")
 
+    training_generator = torch.utils.data.DataLoader(train_set, **parameters)
+    validation_generator = torch.utils.data.DataLoader(val_set, **parameters)
+
+    test_signals = []
+    test_labels = []
+    test_IDs = []
+    test_index = []
+
+    for signals, labels, IDs, index in test_set:
+        test_signals.append(signals)
+        test_labels.append(labels)
+        test_IDs.append(IDs)
+        test_index.append(index)
+
+    # listas a tensores
+    test_signals = torch.stack(test_signals)
+    test_labels = torch.stack(test_labels)
+    test_IDs = torch.stack(test_IDs)
+    test_index = torch.stack(test_index)
+    
+    # Guardado en un archivo .pt
+    torch.save({'data': test_signals, 'labels': test_labels,'patient_ids': test_IDs,'index':test_index} ,'data_UCI/test_set.pt')
+    print("data_UCI/test_set.pt guardado")
+    #Subset para testeo de modelos sin entrenamiento completo
+    """
+    subset_size = int(0.5 * len(dataset))  # 10%
+    subset_indices = np.random.choice(len(dataset), subset_size, replace=False)
+    subset = torch.utils.data.Subset(dataset, subset_indices)
+    subset_loader = torch.utils.data.DataLoader(subset, **parameters)
+    """
 
     # Crear el modelo
-    #model=InceptionTime(c_in=2, c_out=2, seq_len=None, n_filters=8, nb_filters=3)
-    model=Modelo_Convolucional(in_channels=2,out_channels=2, long_signal=250)
-    model = model.to(device)  # Mueve el modelo a la GPU o CPU según corresponda
-    # Se Define el optimizador y la función de pérdida
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+    #model=InceptionTime(c_in=2, c_out=2, seq_len=None, n_filters=32)
+    #model=Modelo_Convolucional(in_channels=2,out_channels=2, long_signal=250)
+    model=Modelo_ConvolucionalV1(in_channels=2,out_channels=2, long_signal=1250)
+    #model=Modelo_ConvolucionalV2(in_channels=2,out_channels=2, long_signal=1250)
+    # Añade esto después de crear el modelo
+    """
+    for layer in model.modules():
+        if isinstance(layer, (torch.nn.Conv1d, torch.nn.Linear)):
+            torch.nn.init.xavier_uniform_(layer.weight)
+            torch.nn.init.zeros_(layer.bias)
+    """   
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)  # Mueve el modelo a la GPU
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay= 1e-4)
+    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=5, min_lr=1e-6, verbose=True)    
     criterion = torch.nn.MSELoss()  # MSELoss para regresión
-    #Parametros de entrenamiento
-    global best_valid_loss
-    best_valid_loss = float('inf')  
-    max_epochs, best_valid_loss = 100, np.inf
-    patience = 5
-    epochs_no_improve = 0
-    """
-    torch.autograd.set_detect_anomaly(True)
-    with torch.no_grad():
-        for local_batch, local_labels in training_generator:
-            local_batch = local_batch.to(device)
-            out = model(local_batch)
-            print("Output inicial:", out[3])
-            break
-    """
-    running_loss = np.zeros(shape=(max_epochs, 2))  # Para almacenar las pérdidas de entrenamiento y validación
-    for epoch in tqdm(range(max_epochs), desc="Entrenamiento"):
-        # Training
-        train_loss, valid_loss = 0.0, 0.0
-        for local_batch, local_labels in training_generator:
-            optimizer.zero_grad() # Reset gradients
-            # Transfer to GPU
-            local_batch, local_labels = local_batch.to(device), local_labels.to(device)
-            preds = model.forward(local_batch) # Realiza la predicción
-            print(preds.dtype, local_labels.dtype)
-            loss = criterion(preds, local_labels) # Calcula la pérdida
-            
-            if torch.isnan(loss):
-                print("¡Loss con NaN detectado! Abortando...")
-                print("Preds:", preds[0])
-                print("Labels:", local_labels[0])
-                exit()
-            
-            loss.backward() # Calcula los gradientes mediante backpropagation
-            for name, param in model.named_parameters():
-                if torch.isnan(param.grad).any():
-                    print(f"⚠️ Gradiente con NaN en: {name}")
-                if torch.isinf(param.grad).any():
-                    print(f"⚠️ Gradiente infinito en: {name}")
 
-            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0) #gradient clipping
-            optimizer.step() # Actualiza los parámetros del modelo
-            train_loss += loss.item()  # Acumula la pérdida de entrenamiento
-            print("Predicción:", preds[0])
-            print("Etiqueta:", local_labels[0])
+    # ENTRENAMIENTO
+    def train_one_step(batch):
+        optimizer.zero_grad() # Reinicia los gradientes
+        data, labels, _, _ = batch # Obtiene los datos y etiquetas
+        data, labels = data.to(device), labels.to(device) # Mueve los datos y etiquetas a la GPU
+        
+        #print("Rango de datos:", torch.min(data).item(), torch.max(data).item())
 
-        with torch.set_grad_enabled(False):
-            for local_batch, local_labels in validation_generator:
-                # Transfer to GPU
-                local_batch, local_labels = local_batch.to(device), local_labels.to(device)
-                preds = model.forward(local_batch)
-                loss = criterion(preds, local_labels)
-                valid_loss += loss.item()
-                
-        if valid_loss < best_valid_loss:
-            best_valid_loss = valid_loss
-            epochs_no_improve = 0
-            torch.save({
-            'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'loss': valid_loss,
-            'model_config': {'c_in': 2, 'c_out': 2, 'n_filters': 8}  # Parámetros de InceptionTime
-            },'best_model.pt')
-        else:
-            epochs_no_improve += 1
-            if epochs_no_improve == patience:
-                print("Early Stopping!")
-                break
-        running_loss[epoch] = [train_loss / len(training_generator), valid_loss / len(validation_generator)]
+        #for name, param in model.named_parameters():
+        #    print(f"{name}: mean={param.mean().item():.4f}, std={param.std().item():.4f}")
+
+        preds = model.forward(data) # Realiza la predicción
+        #print("Preds:", preds[0])
+        #print("Labels:", labels[0])
+        loss = criterion(preds, labels) # Calcula la pérdida
+        """
+        if torch.isnan(loss):
+            print("¡Loss con NaN detectado! Abortando...")
+            print("Preds:", preds[0])
+            print("Labels:", labels[0])
+            exit()
+        """
+        loss.backward() # Calcula los gradientes mediante backpropagation
+        # Añade gradient clipping (busca evitar explosión de gradiente)
+        #torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+        optimizer.step() # Actualiza los parámetros del modelo
+        return loss.item() # Devuelve la pérdida
+
+    def evaluate_one_step(batch):
+        with torch.no_grad():
+            data, labels, _, _ = batch
+            data, labels = data.to(device), labels.to(device)
+            preds = model.forward(data)
+            loss = criterion(preds, labels)
+            return loss.item()
     
-    fig, ax = plt.subplots(figsize=(10, 10), tight_layout=True)
-    ax.plot(running_loss[:, 0], label='Entrenamiento')
-    ax.plot(running_loss[:, 1], label='Validación')
-    ax.set_title('Pérdida en Entrenamiento y Validación')
+    def train_one_epoch():    
+        train_loss, valid_loss = 0.0, 0.0
+
+        model.train()
+        for batch in training_generator:
+        #for batch in subset_loader:
+            train_loss += train_one_step(batch)
+        
+
+        model.eval()    
+        for batch in validation_generator:
+        #for batch in subset_loader:
+            valid_loss += evaluate_one_step(batch)
+
+        return train_loss/len(training_generator), valid_loss/len(validation_generator)
+    
+    max_epochs = 100
+    best_valid_loss = float('inf') 
+    running_loss = np.zeros(shape=(max_epochs, 2))
+    min_delta = 0.00002  # mejora mínima requerida
+    patience = 5
+    patience_significativa = 5
+  
+    for epoch in tqdm(range(max_epochs)):
+        train_l, valid_l = train_one_epoch()
+        running_loss[epoch] = (train_l,valid_l)
+        print(f"[Época {epoch}] Train Loss: {train_l:.6f} - Valid Loss: {valid_l:.6f}")
+        
+        improvement = best_valid_loss - valid_l
+
+        if valid_l < best_valid_loss:
+            best_valid_loss = valid_l
+            no_improvement_count = 0  
+            torch.save({
+                'epoca': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': valid_l
+            }, 'best_model_conv_v1.pt')
+            print(f"Nuevo mejor modelo guardado (valid_loss = {valid_l:.6f})")
+
+            
+            if improvement > min_delta:
+                no_significant_improvement_count = 0
+            else:
+                no_significant_improvement_count += 1
+        else:
+            no_improvement_count += 1
+            no_significant_improvement_count += 1
+
+        
+        if no_improvement_count >= patience:
+            print(f"Early stopping por falta de mejoras (últimas {patience} épocas).")
+            break
+        if no_significant_improvement_count >= patience_significativa:
+            print(f"Early stopping por falta de mejoras > {min_delta:.4f} (últimas {patience_significativa} épocas).")
+            break
+
+        scheduler.step(valid_l)
+
+    fig, ax = plt.subplots(figsize=(7, 4), tight_layout=True)
+    ax.plot(running_loss[:epoch, 0], label='Entrenamiento')
+    ax.plot(running_loss[:epoch, 1], label='Validación')
     ax.set_xlabel('Epoch')
     ax.set_ylabel('Loss')
     ax.legend()
-    plt.xlim(-1, max_epochs)
-    plt.ylim(-2, 2)
     plt.savefig('loss_curve.png')
     plt.show()
+
+if __name__ == '__main__':
+    main()
 
 
     

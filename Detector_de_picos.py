fe48af22e4238d038e661abac2009893480b94c7
JuanGrigolatto <112011408+JuanGrigolatto@users.noreply.github.com>
2025-08-04 16:00:42 -0300

    cambios menores en detector de picos

--- Detector_de_picos.py
+++ Detector_de_picos.py
@@ -37,7 +37,37 @@ plt.show()
 for measure in m.keys():
     print('%s: %f' %(measure, m[measure]))
 """
+def filtrar_ppg(senial_ppg):
 
+    orden = 4
+    frec_sup = 21
+    frec_inf = 0.5
+
+    frecs_corte = [frec_inf, frec_sup]
+    b, a = signal.butter(orden, frecs_corte, 'bandpass', fs = 125)
+
+    w, h = signal.freqz(b,a, worN=8000, fs=125)
+
+    ppg_filtrada = signal.filtfilt(b,a, ppg_muestras[i].numpy())
+
+    """
+    plt.subplot(2, 1, 2)
+    plt.plot(ppg_filtrada, label='Señal PPG', color='blue')
+    plt.xlabel('Muestras')
+    plt.ylabel('Amplitud')
+    plt.title('Señal filtrada')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+    """
+     # Línea de base estimada con media móvil
+    baseline = np.convolve(ppg_filtrada, np.ones(125)/125, mode='same')
+
+    # Señal sin línea de base
+    ppg_filtrada = ppg_filtrada - baseline 
+    
+    return ppg_filtrada
 
 def filtrar_picos_por_distancia(peaks, min_distance = 40):
 
@@ -49,20 +79,48 @@ def filtrar_picos_por_distancia(peaks, min_distance = 40):
     
     return picos_filtrados
 
-def extraer_pulso_fixed_length(signal, fs=125, ancho_pulso=100):
+def extraer_pulso_ecg_fixed_length(signal, fs=125, ancho_pulso=100):
+        wd, m = hp.process(signal, fs)
+        peaks = wd['peaklist']
+
+        intervalos_rr = wd['rr_list']  # Lista de intervalos RR en milisegundos
+
+        frec_cardiaca = m['bpm'] / 60  # Frecuencia cardiaca en Hz
+
+        intervalos_rr = intervalos_rr / 1000  # Convertir a segundos
+        intervalos_rr = intervalos_rr * fs  # Convertir a muestras
+
+        print(f"Intervalos RR: {intervalos_rr}")
+        print(f"Frecuencia cardiaca: {frec_cardiaca:.2f} Hz")
+        print(f"Picos detectados ECG: {len(peaks)}")
+
+        window_pre = int(0.4 * fs)  # Ventana de 0.4 segundos antes del pico
+        window_post = int(0.6 * fs)  # Ventana de 0.6 segundos después del pico
+
+        
+
+
+        if len(peaks) < 2:
+            return None
+        
+    
+
+    
+def extraer_pulso_ppg_fixed_length(signal, fs=125, ancho_pulso=100):
     # Procesar con heartpy para obtener picos sistólicos
    
     try:
         wd, m = hp.process(signal, fs)
-        bpm = m['bpm']
-        print(m.keys())
+        wd_f, _ = hp.process(-signal, fs)  # Señal invertida para detectar foots
+        #bpm = m['bpm']
         peaks = wd['peaklist']
+        foots = wd_f['peaklist']
     except:
         return None  
     
     if len(peaks) < 2:
         return None
-    
+    """"
     if bpm > 0: 
         rr_s = 60 / bpm
     else: 
@@ -70,8 +128,10 @@ def extraer_pulso_fixed_length(signal, fs=125, ancho_pulso=100):
 
     window_pre = int(0.4 * fs *rr_s)
     foots = []
-
-    peaks = filtrar_picos_por_distancia(peaks) 
+    """
+    peaks = filtrar_picos_por_distancia(peaks)
+    foots = filtrar_picos_por_distancia(foots) 
+    """
     # Detección de foots respecto a los picos
     for pico in peaks:
         # max evita que sea negativo el valor
@@ -79,11 +139,11 @@ def extraer_pulso_fixed_length(signal, fs=125, ancho_pulso=100):
         sub = signal[ini:pico]
         if len(sub) > 0:
             foots.append(ini + np.argmin(sub))
-    
+    """
     # Verifica que haya al menos un pulso completo
     if len(foots) < 2:
         return None
-
+    
     pulsos_segmented = []
     
     for i in range(len(foots)-1):
@@ -99,25 +159,27 @@ def extraer_pulso_fixed_length(signal, fs=125, ancho_pulso=100):
             pulso_seg[:largo_actual] = pulso
 
         pulsos_segmented.append(pulso_seg)
-
+    
     return pulsos_segmented, peaks, foots
+
 
 # === Cargar una muestra de tu dataset
 archivo = 'data_UCI/dataset_parte_2.pt'
 data = torch.load(archivo)
 ppg_muestras = data['data'][:, 0, :]  # Todas las muestras, canal PPG
 
-i=1047
+i=10001
 
 #ppg_muestras_ruido = white_noise_torch(ppg_muestras[i], 10)
+ppg_filtrada = filtrar_ppg(ppg_muestras[i].numpy())
+pulsos_segmented, peaks, foots = extraer_pulso_ppg_fixed_length(ppg_filtrada)
 
-pulsos_segmented, peaks, foots = extraer_pulso_fixed_length(ppg_muestras[i].numpy())
 
 if pulsos_segmented is not None:
     plt.figure(figsize=(12, 5))
-    plt.plot(ppg_muestras[i].numpy(), label='Señal PPG', color='blue')
-    plt.plot(peaks, ppg_muestras[i][peaks].numpy(), 'ro', label='Picos')
-    plt.plot(foots, ppg_muestras[i][foots].numpy(), 'go', label='Foots')
+    plt.plot(ppg_filtrada, label='Señal PPG', color='blue')
+    plt.plot(peaks, ppg_filtrada[peaks], 'ro', label='Picos')
+    plt.plot(foots, ppg_filtrada[foots], 'go', label='Foots')
     #plt.plot(ppg_muestras_ruido.numpy(), label='Señal PPG', color='blue')
     #plt.plot(peaks, ppg_muestras_ruido[peaks].numpy(), 'ro', label='Picos')
     #plt.plot(foots, ppg_muestras_ruido[foots].numpy(), 'go', label='Foots')
@@ -128,64 +190,11 @@ if pulsos_segmented is not None:
     plt.grid(True)
     plt.tight_layout()
     plt.show()
-    """
-    for j in range(len(pulsos_segmented)):
-        plt.figure(figsize=(12, 5))
-        plt.plot(pulsos_segmented[j], label=f'pulso {j} de muestra {i}', color='blue')
-        plt.xlabel('Muestras')
-        plt.ylabel('Amplitud')
-        plt.title(f'pulso {j} de muestra {i}')
-        plt.legend()
-        plt.grid(True)
-        plt.tight_layout()
-        plt.show()
-    """
+    print(f"Se detectaron {len(pulsos_segmented)} pulsos en la señal.")
+    print(f"Picos detectados: {len(peaks)}, Foots detectados: {len(foots)}")
 else:
     print("Todo lo que pudo fallar lo hizo")
 
-# filtrado 
-orden = 4
-frec_sup = 21
-frec_inf = 0.5
 
-frecs_corte = [frec_inf, frec_sup]
-b, a = signal.butter(orden, frecs_corte, 'bandpass', fs = 125)
-
-w, h = signal.freqz(b,a, worN=8000, fs=125)
-
-plt.semilogx(w, 20 * np.log10(abs(h)))
-plt.title('Butterworth filter frequency response')
-plt.xlabel('Frequency [rad/s]')
-plt.ylabel('Amplitude [dB]')
-plt.margins(0, 0.1)
-plt.grid(which='both', axis='both')
-plt.axvline(frec_inf, color='green') # cutoff inf frequency
-plt.axvline(frec_sup, color='green') # cutoff sup frequency
-plt.show()
-
-ppg_filtrada = signal.filtfilt(b,a, ppg_muestras[i].numpy())
-
-plt.figure(1)
-plt.subplot(2, 1, 1)
-plt.plot(ppg_muestras[i].numpy(), label='Señal PPG', color='blue')
-plt.plot(ppg_filtrada, label='Señal PPG', color='red')
-plt.xlabel('Muestras')
-plt.ylabel('Amplitud')
-plt.title('Señal sin filtrar')
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-
-#sos = signal.butter(orden, frecs_corte, 'bandpass', fs = 125, output='sos')
-
-plt.subplot(2, 1, 2)
-plt.plot(ppg_filtrada, label='Señal PPG', color='blue')
-plt.xlabel('Muestras')
-plt.ylabel('Amplitud')
-plt.title('Señal filtrada')
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-plt.show()
 
 
